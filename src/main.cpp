3839c05da99fe8b2e6649cd413eb05645c985598
wettestsock <evilsharkeater@gmail.com>
2024-12-26 22:13:15 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "esp_gap_bt_api.h"
 #include "esp_bt_device.h"
 #include "esp_bt.h"
+#include "esp_spp_api.h"
 
 
 #define LED 2
@@ -12,6 +13,23 @@
 #define ON 1
 #define FLASH 2
 
+
+#define TAG "BT_SPP_SERVER"
+
+// This is a callback to handle SPP events
+static void spp_callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
+    switch (event) {
+        case ESP_SPP_INIT_EVT:
+            ESP_LOGI(TAG, "SPP Initialized");
+            esp_spp_start_srv(ESP_SPP_SEC_AUTHENTICATE, ESP_SPP_ROLE_SLAVE, 1, NULL); // Start SPP service
+            break;
+        case ESP_SPP_DATA_IND_EVT:
+            ESP_LOGI(TAG, "Data received: %s", param->data_ind.data);
+            break;
+        default:
+            break;
+    }
+}
 
 uint8_t curr_state = OFF; //led_
 TaskHandle_t flash_led_task = NULL;
@@ -55,15 +73,23 @@ void setup(){
     pinMode(LED,OUTPUT);
 
 
+    // Initialize Bluetooth
     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    esp_bt_controller_init(&bt_cfg);
+    esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT); // Enable Bluetooth Classic mode
+    esp_bluedroid_init();
+    esp_bluedroid_enable();
 
-    Serial.printf("[ ESP BT CONTROLLER INIT %s ]\n", (esp_bt_controller_init(&bt_cfg)) ? "FAIL" : "SUCCESS");
-    Serial.printf("[ ESP BT CONTROLLER ENABLE %s ]\n", (esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT)) ? "FAIL" : "SUCCESS");
-    Serial.printf("[ ESP BLUEDROID INIT %s ]\n", (esp_bluedroid_init()) ? "FAIL" : "SUCCESS");
-    Serial.printf("[ ESP BLUEDROID ENABLE %s ]\n", (esp_bluedroid_enable()) ? "FAIL" : "SUCCESS");
-    Serial.printf("[ ESP BT NAME %s ]\n", (esp_bt_dev_set_device_name("Phone Lamp")) ? "FAIL" : "SUCCESS");
+    // Register the SPP callback
+    esp_spp_register_callback(spp_callback);
 
-    
+    // Start Bluetooth SPP service
+    esp_spp_init(ESP_SPP_MODE_CB);
+
+    // Set Bluetooth device name
+    esp_bt_dev_set_device_name("ESP32_SPP_Server");
+
+    ESP_LOGI(TAG, "Bluetooth SPP Server Initialized");
 
 
     // Disable Bluetooth advertising (hides the device)
