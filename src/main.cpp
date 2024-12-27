1987cb3c0d837a688a0ff3d96cf8e574b663c0f1
wettestsock <evilsharkeater@gmail.com>
2024-12-26 23:54:30 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -123,7 +123,7 @@ void loop()
         if(!strcmp(mac.c_str(), device.getAddress().toString().c_str())){
             Serial.printf("PHONE FOUND STRENGTH: %i\n", 100+device.getRSSI());
         }
-
+        Serial.println(device.getManufacturerData().c_str());
         Serial.printf("%i %s %s %s\n", device.getRSSI(),device.getName().c_str(), device.getAddress().toString().c_str(), device.getServiceUUID().toString().c_str());
     }
 
