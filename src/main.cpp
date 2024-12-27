b9324f73fad9effddf35d370101fd7f85959fd0e
wettestsock <evilsharkeater@gmail.com>
2024-12-26 20:49:37 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -10,31 +10,73 @@
 
 uint8_t status = OFF; //led_
 uint8_t flash_led_state = OFF;
-TaskHandle_t flash_led_task = NULL;
+
+
+// DEBUG SERIAL INPUT
+String input_string(){
+    String out;
+
+
+    while(1){
+        if(Serial.available() >0){
+            char curr = Serial.read();
+            //str.trim();
+            switch(curr){
+                case '\n':
+                    if(out.length()>1){
+
+                        Serial.println();
+                        return out;
+                    }
+                    out = "";
+                break;
+                case '\b':
+                    out.remove(out.length()-1);
+                    Serial.print('\b');
+                    Serial.print(' ');
+                    Serial.print('\b');
+                break;
+                default:
+                    out += curr;
+                    Serial.print(curr);
+                break;
+            }
+        }
+    }
+}
+
 
 void flash_led(void* args){
-    flash_led_state = ON; 
     while(status == FLASH){
         digitalWrite(LED,HIGH);
         vTaskDelay(1000/portTICK_PERIOD_MS);
         digitalWrite(LED,LOW);
         vTaskDelay(1000/portTICK_PERIOD_MS);
     }
-    flash_led_state = OFF;
+    flash_led_state = OFF;  
     vTaskDelete(NULL);
 }
 
 void led(const uint8_t state){
     status = state;
 
-    if(state == FLASH && flash_led_state != ON){
-        xTaskCreate(flash_led, "led flash", configMINIMAL_STACK_SIZE, NULL, 1, &flash_led_task);
+    if(state == FLASH && flash_led_state == OFF){ 
+        flash_led_state = ON;
+        xTaskCreate(flash_led, "led flash", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     } else if (state == ON){
-        vTaskDelete(flash_led_task);
+        flash_led_state = OFF;
+        while(flash_led_state){
+
+        };
         digitalWrite(LED,HIGH);
+        
     } else if (state == OFF){
-        vTaskDelete(flash_led_task);
+        flash_led_state = OFF;
+        while(flash_led_state){
+
+        };
         digitalWrite(LED,LOW);
+
     }
 }
 
@@ -46,13 +88,13 @@ void setup(){
 
 
 void loop(){
-        led(ON);
         Serial.println("[ LED ON ]");
+        led(ON);
         vTaskDelay(2000/portTICK_PERIOD_MS);
-        led(OFF);
         Serial.println("[ LED OFF ]");
+        led(OFF);
         vTaskDelay(2000/portTICK_PERIOD_MS);
-        led(FLASH);
         Serial.println("[ LED FLASH ]");
+        led(FLASH);
         vTaskDelay(2000/portTICK_PERIOD_MS);
 }
