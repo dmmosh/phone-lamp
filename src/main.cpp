fba2200e555e94e90de1a7aab2a3af825ff17edf
wettestsock <evilsharkeater@gmail.com>
2024-12-26 22:54:48 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,48 @@
 #define SERVICE_UUID "9f46b94c-9574-4f6c-bd1b-ddc3a7a83a43"
 #define CHARACTERISTIC_UUID "afe8ef56-902f-4b38-a6a2-0eade0aca572"
 #define LED 2
+#define OFF 0
+#define ON 1
+#define FLASH 2
 
 // Variable to track connection status
 bool deviceConnected = false;
 String mac;
 BLEScan *scan;
+uint8_t curr_state = OFF; //led_
+TaskHandle_t flash_led_task = NULL;
+
+
+
+void flash_led(void* args){
+    while(curr_state == FLASH){
+        digitalWrite(LED,HIGH);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        digitalWrite(LED,LOW);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+    vTaskDelete(NULL);
+}
+
+void led(const uint8_t new_state){  
+    // state: new state
+    // 
+    if(new_state == FLASH && curr_state != FLASH){ 
+        xTaskCreate(flash_led, "led flash", configMINIMAL_STACK_SIZE, NULL, 1, &flash_led_task);
+    } else if (new_state == ON && curr_state != ON){
+        if(curr_state == FLASH){
+            vTaskDelete(flash_led_task);
+        }
+        digitalWrite(LED,HIGH);
+        
+    } else if (new_state == OFF && curr_state != OFF){
+        if(curr_state == FLASH){
+            vTaskDelete(flash_led_task);
+        }
+        digitalWrite(LED,LOW);
+    }
+    curr_state = new_state;
+}
 
 // BLE Server callback class
 class MyServerCallbacks : public BLEServerCallbacks
@@ -19,7 +56,7 @@ class MyServerCallbacks : public BLEServerCallbacks
     void onConnect(BLEServer *pServer, esp_ble_gatts_cb_param_t *param)
     {
         deviceConnected = true;
-        mac = ((BLEDevice*)pServer->getPeerDevices(false)[1].peer_device)->getAddress().toString().c_str();
+        //mac = ((BLEDevice*)pServer->getPeerDevices(false)[0].peer_device)->getAddress().toString().c_str();
         
         Serial.println("Device connected.");
     }
@@ -52,17 +89,19 @@ void setup()
     pAdvertising->addServiceUUID(SERVICE_UUID);
     BLEDevice::startAdvertising();
 
+    led(FLASH);
     while (!deviceConnected){
         Serial.println("Waiting for device to connect...");
         vTaskDelay(500/portTICK_PERIOD_MS);
     }
 
     BLEDevice::deinit();
-    Serial.println(mac);
+    //Serial.println(mac);
 
     pinMode(LED, OUTPUT);
     digitalWrite(LED, HIGH);
 
+    led(ON);
     scan = BLEDevice::getScan();
     scan->setActiveScan(true);
     scan->setInterval(100);
