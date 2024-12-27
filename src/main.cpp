1ac2e60f0507fee59eeb1711039de3421eb064ea
wettestsock <evilsharkeater@gmail.com>
2024-12-26 20:31:25 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,100 @@
 #include <Arduino.h>
-#include "BLEScan.h"
-#include "BLEAdvertisedDevice.h"
-#include "BLEDevice.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "BluetoothSerial.h"
 
 #define LED 2
+#define OFF 0
+#define ON 1
+#define FLASH 2
+
+uint8_t status = OFF; //led_
+uint8_t flash_led_state = OFF;
 
 
+// DEBUG SERIAL INPUT
+String input_string(){
+    String out;
 
-void setup()
-{
-    Serial.begin(115200);
 
-    SerialBT.begin("ESP32_BT_Scanner");  // Start Bluetooth with a device name
+    while(1){
+        if(Serial.available() >0){
+            char curr = Serial.read();
+            //str.trim();
+            switch(curr){
+                case '\n':
+                    if(out.length()>1){
 
+                        Serial.println();
+                        return out;
+                    }
+                    out = "";
+                break;
+                case '\b':
+                    out.remove(out.length()-1);
+                    Serial.print('\b');
+                    Serial.print(' ');
+                    Serial.print('\b');
+                break;
+                default:
+                    out += curr;
+                    Serial.print(curr);
+                break;
+            }
+        }
+    }
 }
 
 
-BluetoothSerial SerialBT; // Bluetooth serial object
-
-void loop()
-{
-    
-
-
-    int deviceCount = SerialBT.scanDevices(); // Start scanning for Bluetooth devices
-  
-  if (deviceCount == 0) {
-    Serial.println("No devices found.");
-  } else {
-    Serial.println("Devices found:");
-    for (int i = 0; i < deviceCount; i++) {
-        Serial.printf("device mac: %s\n",SerialBT.getBtAddressString());
+void flash_led(void* args){
+    while(status == FLASH){
+        digitalWrite(LED,HIGH);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        digitalWrite(LED,LOW);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
     }
-  }
+    flash_led_state = OFF;  
+    vTaskDelete(NULL);
+}
+
+void led(const uint8_t state){
+    status = state;
+
+    if(state == FLASH && flash_led_state == OFF){ 
+        flash_led_state = ON;
+        xTaskCreate(flash_led, "led flash", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    } else if (state == ON){
+        flash_led_state = OFF;
+        while(flash_led_state){
+
+        };
+        digitalWrite(LED,HIGH);
+        
+    } else if (state == OFF){
+        flash_led_state = OFF;
+        while(flash_led_state){
+
+        };
+        digitalWrite(LED,LOW);
+
+    }
+}
+
+void setup(){
+    Serial.begin(115200);
+    pinMode(LED,OUTPUT);
+    
+}   
+
+
+void loop(){
+    Serial.println("Enter command:");
+    String input = input_string();
+    if(!strcmp(input.c_str(), "on")){
+        led(ON);
+    } else if (!strcmp(input.c_str(), "off")){
+        led(OFF);
+    } else if (!strcmp(input.c_str(), "flash")){
+        led(FLASH);
+    }
 }
