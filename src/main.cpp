87b2de23952aae7634915b02f3301c80e9e70eb1
wettestsock <evilsharkeater@gmail.com>
2024-12-26 22:44:15 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,89 @@
 #include <Arduino.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "BLEScan.h"
+#include "BLEAdvertisedDevice.h"
+#include "BLEDevice.h"
 #include "BluetoothSerial.h"
 
+#define SERVICE_UUID "9f46b94c-9574-4f6c-bd1b-ddc3a7a83a43"
+#define CHARACTERISTIC_UUID "afe8ef56-902f-4b38-a6a2-0eade0aca572"
 #define LED 2
-#define OFF 0
-#define ON 1
-#define FLASH 2
 
+// Variable to track connection status
+bool deviceConnected = false;
+String mac;
+BLEScan *scan;
 
-BluetoothSerial SerialBT;
-uint8_t curr_state = OFF; //led_
-TaskHandle_t flash_led_task = NULL;
-
-
-
-void flash_led(void* args){
-    while(curr_state == FLASH){
-        digitalWrite(LED,HIGH);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-        digitalWrite(LED,LOW);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-    }
-    vTaskDelete(NULL);
-}
-
-void led(const uint8_t new_state){  
-    // state: new state
-    // 
-    if(new_state == FLASH && curr_state != FLASH){ 
-        xTaskCreate(flash_led, "led flash", configMINIMAL_STACK_SIZE, NULL, 1, &flash_led_task);
-    } else if (new_state == ON && curr_state != ON){
-        if(curr_state == FLASH){
-            vTaskDelete(flash_led_task);
-        }
-        digitalWrite(LED,HIGH);
+// BLE Server callback class
+class MyServerCallbacks : public BLEServerCallbacks
+{
+    void onConnect(BLEServer *pServer, esp_ble_gatts_cb_param_t *param)
+    {
+        deviceConnected = true;
+        mac = ((BLEDevice*)pServer->getPeerDevices(false)[0].peer_device)->getAddress().toString().c_str();
         
-    } else if (new_state == OFF && curr_state != OFF){
-        if(curr_state == FLASH){
-            vTaskDelete(flash_led_task);
-        }
-        digitalWrite(LED,LOW);
+        Serial.println("Device connected.");
     }
-    curr_state = new_state;
+
+    void onDisconnect(BLEServer *pServer)
+    {
+        deviceConnected = false;
+        Serial.println("Device disconnected.");
+    }
+};
+
+void setup()
+{
+    Serial.begin(115200);
+    BLEDevice::init("Phone Lamp");
+
+    BLEServer *pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
+
+    // pServer->getAdvertising()->setScanFilter(1,0);
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID,
+        BLECharacteristic::PROPERTY_READ |
+            BLECharacteristic::PROPERTY_WRITE);
+
+    pService->start();
+    // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    BLEDevice::startAdvertising();
+
+    while (!deviceConnected){
+        Serial.println("Waiting for device to connect...");
+        vTaskDelay(500/portTICK_PERIOD_MS);
+    }
+
+    BLEDevice::deinit();
+    Serial.println(mac);
+
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, HIGH);
+
+    scan = BLEDevice::getScan();
+    scan->setActiveScan(true);
+    scan->setInterval(100);
+    scan->setWindow(99);
+    BLEDevice::init("");
 }
 
-void setup(){
-    Serial.begin(115200);
-    pinMode(LED,OUTPUT);
 
-    
-    SerialBT.begin("Phone Lamp");
-    
-}   
+BluetoothSerial SerialBT; // Bluetooth serial object
 
+void loop()
+{
+    BLEScanResults results = scan->start(1);
+    for (size_t i = 0; i < results.getCount(); i++)
+    {
+        BLEAdvertisedDevice device = results.getDevice(i);
+        if(!strcmp(mac.c_str(), device.getAddress().toString().c_str())){
+            Serial.printf("PHONE FOUND STRENGTH: %i\n", 100+device.getRSSI());
+        }
 
-void loop(){
+        Serial.printf("%i %s %s %s\n", device.getRSSI(),device.getName().c_str(), device.getAddress().toString().c_str(), device.getServiceUUID().toString().c_str());
+    }
 
-    BTScanResults* devices = SerialBT.getScanResults();
-
-    Serial.println(devices->getCount());
-    
 }
