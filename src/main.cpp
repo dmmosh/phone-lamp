61195a630c9641689fc4f6aae292d47f7dcc405e
wettestsock <evilsharkeater@gmail.com>
2024-12-26 21:08:34 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -12,40 +12,6 @@ uint8_t curr_state = OFF; //led_
 TaskHandle_t flash_led_task = NULL;
 
 
-// DEBUG SERIAL INPUT
-String input_string(){
-    String out;
-
-
-    while(1){
-        if(Serial.available() >0){
-            char curr = Serial.read();
-            //str.trim();
-            switch(curr){
-                case '\n':
-                    if(out.length()>1){
-
-                        Serial.println();
-                        return out;
-                    }
-                    out = "";
-                break;
-                case '\b':
-                    out.remove(out.length()-1);
-                    Serial.print('\b');
-                    Serial.print(' ');
-                    Serial.print('\b');
-                break;
-                default:
-                    out += curr;
-                    Serial.print(curr);
-                break;
-            }
-        }
-    }
-}
-
-
 void flash_led(void* args){
     while(curr_state == FLASH){
         digitalWrite(LED,HIGH);
@@ -86,11 +52,11 @@ void setup(){
 void loop(){
         Serial.println("[ LED ON ]");
         led(ON);
-        vTaskDelay(2000/portTICK_PERIOD_MS);
+        vTaskDelay(5/portTICK_PERIOD_MS);
         Serial.println("[ LED OFF ]");
         led(OFF);
-        vTaskDelay(2000/portTICK_PERIOD_MS);
+        vTaskDelay(5/portTICK_PERIOD_MS);
         Serial.println("[ LED FLASH ]");
         led(FLASH);
-        vTaskDelay(2000/portTICK_PERIOD_MS);
+        vTaskDelay(5/portTICK_PERIOD_MS);
 }
