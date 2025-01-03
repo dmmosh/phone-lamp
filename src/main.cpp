44cf06f4ecbde6bc1c89f4fef1ab142db7f82c3a
wettestsock <evilsharkeater@gmail.com>
2025-01-03 16:27:33 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -162,14 +162,15 @@ void lamp(const uint8_t new_state){
 inline void connect_wait(){
     uint16_t ms_5  = 0;
     uint16_t seconds = 0;
-      pinMode(LED,OUTPUT);
+    pinMode(LED,OUTPUT);
 
     while(!connected){
         
         if(ms_5 >= 200){    
-            if(seconds<11){ // note: at 10 second mark, it sets led to LOW and it stays there afterwards
-            digitalWrite(LED,seconds%2);
+            if(seconds<21){ // note: at 20 second mark, it sets led to LOW and it stays there afterwards (to save power and not be annoying)
+                digitalWrite(LED,seconds%2);
             }
+
             Serial.printf("Waiting for device to pair... %is\n", seconds);
             seconds++;
             ms_5 = 0;
